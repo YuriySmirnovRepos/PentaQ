ec7dc692654da3023afdc6c74b5c0cb0cabded4c
Yuriy Smirnov <yurkadreamer@gmail.com>
2026-04-18 00:18:26 +0300

    Path fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  base: './',
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@core': path.resolve(__dirname, './src/core'),
-      '@features': path.resolve(__dirname, './src/features'),
-      '@shared': path.resolve(__dirname, './src/shared'),
-      '@app': path.resolve(__dirname, './src/app'),
+      '@': '/src',
+      '@core': '/src/core',
+      '@features': '/src/features',
+      '@shared': '/src/shared',
+      '@app': '/src/app',
     },
   },
   build: {
