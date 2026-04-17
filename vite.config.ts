d095ee38aad3be1a7643530d0de18318fefa8213
Yuriy Smirnov <yurkadreamer@gmail.com>
2026-04-17 23:49:22 +0300

    Fixes for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// GitHub Pages требует базовый путь с именем репозитория
+const BASE_PATH = process.env.GITHUB_PAGES === 'true' ? '/PentaQ/' : '/';
+
 export default defineConfig({
+  base: BASE_PATH,
   plugins: [react()],
   base: './',
   resolve: {
